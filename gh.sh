d297c2d7141bc031c40bb6254f8f18512eb519b1
Linus Fernandes <linus.fernandes@gmail.com>
2025-11-15 12:54:18 +0530

    fix: update to add new file gh.sh

---
+++ gh.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+######################################################################
+# @author      : Linus Fernandes (linusfernandes at gmail dot com)
+# @file        : git
+# @created     : Saturday Nov 15, 2025 12:36:03 IST
+#
+# @description :
+######################################################################
+
+function get_owner_and_repo() {
+  local -n output=$1
+  local remote_url=$(git remote get-url origin 2> /dev/null)
+  # Works for both HTTPS and SSH URLs
+  if [[ $remote_url =~ github\.com[:/](.+)/(.+)\.git ]]; then
+    owner="${BASH_REMATCH[1]}"
+    repo="${BASH_REMATCH[2]}"
+    export output=("$owner" "$repo")
+  else
+    echo "❌ Could not parse owner/repo from remote URL." >&2
+    exit 1
+  fi
+}
