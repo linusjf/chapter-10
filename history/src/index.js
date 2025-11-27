4ca9407641345befeb94cf7d94deb330fc0882fd
Linus Fernandes <linus.fernandes@gmail.com>
2025-11-27 19:32:56 +0530

    fix: update kube-config in script before retrieving logs from pods

--- history/src/index.js
+++ history/src/index.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const mongodb = require("mongodb");
-const amqp = require('amqplib');
+const amqp = require("amqplib");
 
 if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT.",
+  );
 }
 
 if (!process.env.DBHOST) {
-    throw new Error("Please specify the database host using environment variable DBHOST.");
+  throw new Error(
+    "Please specify the database host using environment variable DBHOST.",
+  );
 }
 
 if (!process.env.DBNAME) {
-    throw new Error("Please specify the name of the database using environment variable DBNAME");
+  throw new Error(
+    "Please specify the name of the database using environment variable DBNAME",
+  );
 }
 
 if (!process.env.RABBIT) {
-    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
+  throw new Error(
+    "Please specify the name of the RabbitMQ host using environment variable RABBIT",
+  );
 }
 
 const PORT = process.env.PORT;
@@ -23,97 +31,111 @@ const DBHOST = process.env.DBHOST;
 const DBNAME = process.env.DBNAME;
 const RABBIT = process.env.RABBIT;
 
+const health = {
+  initialized: false,
+  markInitialized() {
+    this.initialized = true;
+  },
+  isReady() {
+    return this.initialized;
+  },
+};
+
 //
 // Application entry point.
 //
 async function main() {
+  const app = express();
 
-    const app = express();
+  //
+  // Enables JSON body parsing for HTTP requests.
+  //
+  app.use(express.json());
 
-    //
-    // Enables JSON body parsing for HTTP requests.
-    //
-    app.use(express.json()); 
+  //
+  // Connects to the database server.
+  //
+  const client = await mongodb.MongoClient.connect(DBHOST);
 
-    //
-    // Connects to the database server.
-    //
-    const client = await mongodb.MongoClient.connect(DBHOST);
+  //
+  // Gets the database for this microservice.
+  //
+  const db = client.db(DBNAME);
 
-    //
-    // Gets the database for this microservice.
-    //
-    const db  = client.db(DBNAME);
+  //
+  // Gets the collection for storing video viewing history.
+  //
+  const historyCollection = db.collection("history");
 
-    //
-    // Gets the collection for storing video viewing history.
-    //
-    const historyCollection = db.collection("history");
-    
-    //
-    // Connects to the RabbitMQ server.
-    //
-    const messagingConnection = await amqp.connect(RABBIT); 
+  //
+  // Connects to the RabbitMQ server.
+  //
+  const messagingConnection = await amqp.connect(RABBIT);
 
-    //
-    // Creates a RabbitMQ messaging channel.
-    //
-    const messageChannel = await messagingConnection.createChannel(); 
+  //
+  // Creates a RabbitMQ messaging channel.
+  //
+  const messageChannel = await messagingConnection.createChannel();
 
-    //
-    // Asserts that we have a "viewed" exchange.
-    //
-    await messageChannel.assertExchange("viewed", "fanout"); 
+  //
+  // Asserts that we have a "viewed" exchange.
+  //
+  await messageChannel.assertExchange("viewed", "fanout");
 
-	//
-	// Creates an anonyous queue.
-	//
-	const { queue } = await messageChannel.assertQueue("", { exclusive: true }); 
+  //
+  // Creates an anonyous queue.
+  //
+  const { queue } = await messageChannel.assertQueue("", { exclusive: true });
 
-    console.log(`Created queue ${queue}, binding it to "viewed" exchange.`);
-    
-    //
-    // Binds the queue to the exchange.
-    //
-    await messageChannel.bindQueue(queue, "viewed", ""); 
+  console.log(`Created queue ${queue}, binding it to "viewed" exchange.`);
 
-    //
-    // Start receiving messages from the anonymous queue.
-    //
-    await messageChannel.consume(queue, async (msg) => {
-        console.log("Received a 'viewed' message");
+  //
+  // Binds the queue to the exchange.
+  //
+  await messageChannel.bindQueue(queue, "viewed", "");
 
-        const parsedMsg = JSON.parse(msg.content.toString()); // Parse the JSON message.
-        
-        await historyCollection.insertOne({ videoId: parsedMsg.video.id }); // Record the "view" in the database.
+  //
+  // Start receiving messages from the anonymous queue.
+  //
+  await messageChannel.consume(queue, async (msg) => {
+    console.log("Received a 'viewed' message");
 
-        console.log("Acknowledging message was handled.");
+    const parsedMsg = JSON.parse(msg.content.toString()); // Parse the JSON message.
 
-        messageChannel.ack(msg); // If there is no error, acknowledge the message.
-    });
+    await historyCollection.insertOne({ videoId: parsedMsg.video.id }); // Record the "view" in the database.
 
-    //
-    // HTTP GET route to retrieve video viewing history.
-    //
-    app.get("/history", async (req, res) => {
-        //
-        // Retreives viewing history from database.
-        // In a real application this should be paginated.
-        //
-        const history = await historyCollection.find().toArray(); 
-        res.json({ history });
-    });
+    console.log("Acknowledging message was handled.");
 
+    messageChannel.ack(msg); // If there is no error, acknowledge the message.
+  });
+
+  app.get("/alive", (_, res) => res.sendStatus(200));
+
+  app.get("/ready", (_, res) => {
+    res.sendStatus(health.isInitialized() ? 200 : 503);
+  });
+  //
+  // HTTP GET route to retrieve video viewing history.
+  //
+  app.get("/history", async (req, res) => {
     //
-    // Starts the HTTP server.
+    // Retreives viewing history from database.
+    // In a real application this should be paginated.
     //
-    app.listen(PORT, () => {
-        console.log("Microservice online.");
-    });
+    const history = await historyCollection.find().toArray();
+    res.json({ history });
+  });
+
+  //
+  // Starts the HTTP server.
+  //
+  app.listen(PORT, () => {
+    console.log("Microservice online.");
+    health.markInitialized();
+  });
 }
 
-main()
-    .catch(err => {
-        console.error("Microservice failed to start.");
-        console.error(err && err.stack || err);
-    });
+main().catch((err) => {
+  console.error("Microservice failed to start.");
+  console.error((err && err.stack) || err);
+});
