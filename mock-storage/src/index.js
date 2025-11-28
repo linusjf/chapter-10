9235d7e8fd1b7d65b5d65465b1393e4209052f70
Linus Fernandes <linus.fernandes@gmail.com>
2025-11-28 14:52:25 +0530

    fix: add ready and alive routes to index.js for all other microservices

--- mock-storage/src/index.js
+++ mock-storage/src/index.js
@@ -3,44 +3,60 @@ const fs = require("fs");
 const path = require("path");
 
 if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT.",
+  );
 }
 
 const PORT = process.env.PORT;
 
 const storagePath = path.join(__dirname, "../storage");
 console.log(`Storing files at ${storagePath}.`);
+const health = {
+  initialized: false,
+  markInitialized() {
+    this.initialized = true;
+  },
+  isReady() {
+    return this.initialized;
+  },
+};
 
 const app = express();
 
+app.get("/alive", (_, res) => res.sendStatus(200));
+
+app.get("/ready", (_, res) => {
+  res.sendStatus(health.isReady() ? 200 : 503);
+});
 //
 // HTTP GET route that streams a video from storage.
 //
 app.get("/video", (req, res) => {
-
-    const videoId = req.query.id;
-    const localFilePath = path.join(storagePath, videoId);
-    res.sendFile(localFilePath);
+  const videoId = req.query.id;
+  const localFilePath = path.join(storagePath, videoId);
+  res.sendFile(localFilePath);
 });
 
 //
 // HTTP POST route to upload a video to storage.
 //
 app.post("/upload", (req, res) => {
-
-    const videoId = req.headers.id;
-    const localFilePath = path.join(storagePath, videoId);
-    const fileWriteStream = fs.createWriteStream(localFilePath);
-    req.pipe(fileWriteStream)
-        .on("error", err => {
-            console.error("Upload failed.");
-            console.error(err && err.stack || err);
-        })
-        .on("finish", () => {
-            res.sendStatus(200);
-        });
+  const videoId = req.headers.id;
+  const localFilePath = path.join(storagePath, videoId);
+  const fileWriteStream = fs.createWriteStream(localFilePath);
+  req
+    .pipe(fileWriteStream)
+    .on("error", (err) => {
+      console.error("Upload failed.");
+      console.error((err && err.stack) || err);
+    })
+    .on("finish", () => {
+      res.sendStatus(200);
+    });
 });
 
 app.listen(PORT, () => {
-    console.log(`Microservice online`);
+  console.log(`Microservice online`);
+  health.markInitialized();
 });
--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,62 +1,83 @@
 const express = require("express");
-const amqp = require('amqplib');
+const amqp = require("amqplib");
 const axios = require("axios");
 
 if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT.",
+  );
 }
 
 if (!process.env.RABBIT) {
-    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
+  throw new Error(
+    "Please specify the name of the RabbitMQ host using environment variable RABBIT",
+  );
 }
 
 const PORT = process.env.PORT;
 const RABBIT = process.env.RABBIT;
 
+const health = {
+  initialized: false,
+  markInitialized() {
+    this.initialized = true;
+  },
+  isReady() {
+    return this.initialized;
+  },
+};
 //
 // Application entry point.
 //
 async function main() {
-    const messagingConnection = await amqp.connect(RABBIT); // Connects to the RabbitMQ server.
-    
-    const messageChannel = await messagingConnection.createChannel(); // Creates a RabbitMQ messaging channel.
-    
-    await messageChannel.assertExchange("viewed", "fanout"); // Asserts that we have a "viewed" exchange.
+  const messagingConnection = await amqp.connect(RABBIT); // Connects to the RabbitMQ server.
 
-    //
-    // Broadcasts the "viewed" message to other microservices.
-    //
-    function broadcastViewedMessage(messageChannel, videoId) {
-        console.log(`Publishing message on "viewed" exchange.`);
-            
-        const msg = { video: { id: videoId } };
-        const jsonMsg = JSON.stringify(msg);
-        messageChannel.publish("viewed", "", Buffer.from(jsonMsg)); // Publishes message to the "viewed" exchange.
-    }
+  const messageChannel = await messagingConnection.createChannel(); // Creates a RabbitMQ messaging channel.
 
-    const app = express();
+  await messageChannel.assertExchange("viewed", "fanout"); // Asserts that we have a "viewed" exchange.
 
-    app.get("/video", async (req, res) => { // Route for streaming video.
+  //
+  // Broadcasts the "viewed" message to other microservices.
+  //
+  function broadcastViewedMessage(messageChannel, videoId) {
+    console.log(`Publishing message on "viewed" exchange.`);
 
-        const videoId = req.query.id;
-        const response = await axios({ // Forwards the request to the video-storage microservice.
-            method: "GET",
-            url: `http://video-storage/video?id=${videoId}`, 
-            data: req, 
-            responseType: "stream",
-        });
-        response.data.pipe(res);
+    const msg = { video: { id: videoId } };
+    const jsonMsg = JSON.stringify(msg);
+    messageChannel.publish("viewed", "", Buffer.from(jsonMsg)); // Publishes message to the "viewed" exchange.
+  }
 
-        broadcastViewedMessage(messageChannel, videoId); // Sends the "viewed" message to indicate this video has been watched.
+  const app = express();
+
+  app.get("/alive", (_, res) => res.sendStatus(200));
+
+  app.get("/ready", (_, res) => {
+    res.sendStatus(health.isReady() ? 200 : 503);
+  });
+  app.get("/video", async (req, res) => {
+    // Route for streaming video.
+
+    const videoId = req.query.id;
+    const response = await axios({
+      // Forwards the request to the video-storage microservice.
+      method: "GET",
+      url: `http://video-storage/video?id=${videoId}`,
+      data: req,
+      responseType: "stream",
     });
+    response.data.pipe(res);
 
-    app.listen(PORT, () => { // Starts the HTTP server.
-        console.log("Microservice online.");
-    });
+    broadcastViewedMessage(messageChannel, videoId); // Sends the "viewed" message to indicate this video has been watched.
+  });
+
+  app.listen(PORT, () => {
+    // Starts the HTTP server.
+    console.log("Microservice online.");
+    health.markInitialized();
+  });
 }
 
-main()
-    .catch(err => {
-        console.error("Microservice failed to start.");
-        console.error(err && err.stack || err);
-    });
+main().catch((err) => {
+  console.error("Microservice failed to start.");
+  console.error((err && err.stack) || err);
+});
